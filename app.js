5afd924f2182b426244bef0565b5931296fb1cb2
derekmhawkins <derekmhawkins@gmail.com>
2017-07-27 00:14:09 -0500

    Updated api link from 'http' to 'https'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
   var app = angular.module('f1', []);
 
   app.controller('MainCtrl', function($scope, $http) {
-    $http.get('http://ergast.com/api/f1/2017/5/driverStandings.json').then(function(response) {
+    $http.get('https://ergast.com/api/f1/2017/5/driverStandings.json').then(function(response) {
       $scope.information = response.data;
 
       $scope.column = '';
